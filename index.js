4d404f651f5239cc52456390811bb55f0843c3e1
ivanalbizu <mariano-ivan.gonzalez-albizu@capgemini.com>
2025-01-03 14:52:09 +0100

    set urls checkers

--- index.js
+++ index.js
@@ -13,37 +13,60 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'IBM - Equal Access Checker for Node', message: 'IBM - Equal Access Checker for Node' })
 })
 
+function invertObj(obj) {
+  const ordered = {
+    results: obj.results,
+    nls: obj.nls,
+    ...obj,
+  };
+  let invert_obj = {};
+  reverse_obj = Object.keys(ordered).reverse();
+  reverse_obj.forEach(function (x) {
+    invert_obj[x] = ordered[x];
+  });
+  return invert_obj;
+}
+
 app.post('/', async (req, res) => {
-  const url = req.body.url
-  let domain
-  if (!url) {
-    return res.status(400).send('url no enviada')
-  }
-  console.log(url)
-  if (url) {
-    const url2 = new URL(url)
-    domain = url2.hostname
-  } else {
-    console.log("Invalid URL")
+  let urls = req.body.url
+
+  if (!urls.length) {
+    return res.render('index', {message: 'URLs no válida'})
   }
 
-  try {
-    aChecker.getCompliance(url, domain).then((results) => {
+  let urlArr = urls.split(',').map(item => item.trim())
+  let report = []
+
+  if (!urlArr.length) {
+    urls = req.body.url
+    urlArr = urls.split(',').map(item => item.trim())
+    return res.render('index', {message: 'URLs no válida'})
+  }
+
+  for (const element of urlArr) {
+    try {
+      new URL(element)
+    } catch (error) {
+      console.log(error, '\n', element)
+      delete urlArr[element]
+    }
+  }
+
+  for await (const item of urlArr) {
+    console.log(item)
+    const resultados = await aChecker.getCompliance(item, item.replaceAll('/', '_')).then((results) => {
       const report = results.report
-
-      const returnCode = aChecker.assertCompliance(report)
-
-      console.log("returnCode:", returnCode)
-
-      aChecker.close()
-      res.render('index', report)
+      //const returnCode = aChecker.assertCompliance(report)
+      //console.log("returnCode:", returnCode)
+      return report
     })
-  } catch (err) {
-    console.error(err)
-    res.send('dentro de catch', err)
-  } finally {
-    await aChecker.close()
+    report.push(invertObj(resultados))
   }
+
+  await aChecker.close()
+  console.log({report});
+
+  res.render('index', {report})
 })
 
 app.get('/screenshot', async (req, res) => {
